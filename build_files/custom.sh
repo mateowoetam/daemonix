04a9d2f14126e9c95bff6e40babd608cf9969715
DXC-0 <inscription-site-1@protonmail.com>
2025-10-29 10:04:12 +0100

    commit

--- build_files/custom.sh
+++ build_files/custom.sh
@@ -3,6 +3,3 @@
 # custom name
 sed -i 's/^NAME=.*/NAME="Fedora (Daemonix)"/' /etc/os-release
 sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="Fedora (Daemonix)"/' /etc/os-release
-
-# fastfetch
-cp -r /ctx/fastfetch /etc
