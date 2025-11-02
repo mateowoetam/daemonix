054084b1a8c74426a3e5125443c29e4e98d9e024
DXC-0 <inscription-site-1@protonmail.com>
2025-11-02 22:38:06 +0000

    Update mount-nix-overlay.sh

--- build_files/mount-nix-overlay.sh
+++ build_files/mount-nix-overlay.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/bash
 set -e
 
-if ! lsmod | grep -q overlay; then
-    modprobe overlay || {
-        exit 1
-    }
-fi
-
 mount -t overlay overlay \
   -o lowerdir=/usr/share/nix-store,upperdir=/var/lib/nix-store,workdir=/var/cache/nix-store \
   /nix
