679a6f079920d8c266a3f4c3d0c1acc47e2071e2
Mateo Orlaineta <mateowoetam@proton.me>
2026-01-19 20:02:46 -0600

    Regular Update

--- build_files/default-flatpak-clean.sh
+++ build_files/default-flatpak-clean.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-flatpak -y uninstall \
-  org.fkoehler.KTailctl \
-  org.gnome.DejaDup \
-  org.kde.haruna \
+apps=(
+  org.fkoehler.KTailctl
+  org.gnome.DejaDup
+  org.kde.haruna
   org.mozilla.firefox
+)
+
+installed=$(flatpak list --app --columns=application)
+
+for app in "${apps[@]}"; do
+  if echo "$installed" | grep -qx "$app"; then
+    flatpak -y uninstall "$app"
+  fi
+done
